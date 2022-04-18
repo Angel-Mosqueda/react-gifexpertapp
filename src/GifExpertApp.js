dfdf28ef5bad03e01bbace324959ddf54b0a59b0
Angel-Mosqueda <Angel_Mosqueda_Esparza@hotmail.com>
2022-04-18 11:15:06 -0500

    first commit

---
+++ src/GifExpertApp.js
@@ -0,0 +1,40 @@
+import React, { useState } from 'react';
+import { AddCategory } from './components/AddCategory';
+import { GifGrid } from './components/GifGrid';
+
+export const GifExpertApp = () => {
+
+    // const categories = ['One Punch', 'Samurai X', 'Dragon Ball']
+    const [categories, setCategories] = useState(['One Punch'])
+
+    // const handleAdd = () => {
+    //     // setCategories([...categories, 'HunterXHunter'])
+
+    //     setCategories( cats => [...cats, 'HunterXHunter'])
+    // }
+
+    return (
+        <div>
+            <h2>GifExpertApp</h2>
+            <AddCategory setCategories={setCategories}/>
+            <hr />
+
+            {/* <button onClick={handleAdd}>Agregar</button> */}
+            <ol>
+                {
+                    categories.map(categorie => {
+                        // return <li key={categorie}>{categorie}</li>
+                        
+                        return<GifGrid 
+                            key={categorie}
+                            category={categorie} 
+                        />
+                    })
+                }
+            </ol>
+        </div>
+    )
+
+
+
+}
